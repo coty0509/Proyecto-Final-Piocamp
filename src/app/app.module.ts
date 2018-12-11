2f265ee41b16bcb6862451d5b0eefab4d500c875
coty0509 <coty0509@hotmail.com>
2018-12-10 23:10:12 -0500

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InformacionComponent } from './informacion/informacion.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { FooterPageComponent } from './footer-page/footer-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InformacionComponent
+    InformacionComponent,
+    ContactoComponent,
+    NoticiasComponent,
+    QuienesSomosComponent,
+    ServiciosComponent,
+    FooterPageComponent
   ],
   imports: [
     BrowserModule,
